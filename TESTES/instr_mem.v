001cd9737e7234c1083a59962ab89af6c3e36564
ThiagoPfeifer09 <166036070+ThiagoPfeifer09@users.noreply.github.com>
2025-06-19 12:57:45 -0300

    Update instr_mem.v

--- TESTES/instr_mem.v
+++ TESTES/instr_mem.v
@@ -1,15 +1,18 @@
-module instr_mem (
-    input  wire [31:0] addr,       // endereço de instrução (PC)
-    output wire [31:0] instr       // instrução lida
+// instr_mem.v (COM LEITURA SÍNCRONA - CUIDADO, PODE QUEBRAR SEU PIPELINE ATUAL)
+module instr_mem_synchronous (
+    input  wire        clk,        // Agora depende do clock
+    input  wire [31:0] addr,       // Endereço de instrução (PC)
+    output reg  [31:0] instr       // Instrução lida (agora um 'reg')
 );
-  // RAM
   reg [31:0] memory [0:255];      
 
-  // carrega o conteudo do arquivo para instr_mem
+  // Carrega o conteudo do arquivo para instr_mem
   initial begin
       $readmemh("programa.hex", memory);
   end
 
-  // lê a instrução da memória
-  assign instr = memory[addr[9:2]];  
+  // Lê a instrução da memória de forma SÍNCRONA
+  always @(posedge clk) begin
+      instr <= memory[addr[9:2]]; // A instrução só estará disponível no próximo ciclo de clock
+  end
 endmodule
